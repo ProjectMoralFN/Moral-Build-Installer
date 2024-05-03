bda4c5db8e3c105a8ec45124f0a7eb2fe1f0b364
onbro <167833821+ProjectMoralFN@users.noreply.github.com>
2024-05-02 23:12:56 -0700

    Add files via upload

---
+++ Project Moral 11.31 Installer/Installer.cs
@@ -0,0 +1,94 @@
+﻿using static FileManifest;
+using System.IO.Compression;
+using System.Net;
+
+internal class Installer
+{
+    public static async Task Download(ManifestFile manifest, string version, string resultPath)
+    {
+        long totalBytes = manifest.Size;
+        long completedBytes = 0;
+        int progressLength = 0;
+
+        if (!Directory.Exists(resultPath))
+            Directory.CreateDirectory(resultPath);
+
+        SemaphoreSlim semaphore = new SemaphoreSlim(Environment.ProcessorCount * 2);
+
+        await Task.WhenAll(manifest.Chunks.Select(async chunkedFile =>
+        {
+            await semaphore.WaitAsync();
+
+            try
+            {
+                WebClient webClient = new WebClient();
+
+                string outputFilePath = Path.Combine(resultPath, chunkedFile.File);
+                var fileInfo = new FileInfo(outputFilePath);
+
+                if (File.Exists(outputFilePath) && fileInfo.Length == chunkedFile.FileSize)
+                {
+                    completedBytes += chunkedFile.FileSize;
+                    semaphore.Release();
+                    return;
+                }
+
+                Directory.CreateDirectory(Path.GetDirectoryName(outputFilePath));
+
+                using (FileStream outputStream = File.OpenWrite(outputFilePath))
+                {
+                    foreach (int chunkId in chunkedFile.ChunksIds)
+                    {
+                    retry:
+
+                        try
+                        {
+                            string chunkUrl = Globals.SeasonBuildVersion + $"/{version}/" + chunkId + ".chunk";
+                            var chunkData = await webClient.DownloadDataTaskAsync(chunkUrl);
+
+                            byte[] chunkDecompData = new byte[Globals.CHUNK_SIZE + 1];
+                            int bytesRead;
+                            long chunkCompletedBytes = 0;
+
+                            MemoryStream memoryStream = new MemoryStream(chunkData);
+                            GZipStream decompressionStream = new GZipStream(memoryStream, CompressionMode.Decompress);
+
+                            while ((bytesRead = await decompressionStream.ReadAsync(chunkDecompData, 0, chunkDecompData.Length)) > 0)
+                            {
+                                await outputStream.WriteAsync(chunkDecompData, 0, bytesRead);
+                                Interlocked.Add(ref completedBytes, bytesRead);
+                                Interlocked.Add(ref chunkCompletedBytes, bytesRead);
+
+                                double progress = (double)completedBytes / totalBytes * 100;
+                                string progressMessage = $"\rDownload Status: {ConvertStorageSize.FormatBytesWithSuffix(completedBytes)} / {ConvertStorageSize.FormatBytesWithSuffix(totalBytes)} ({progress:F2}%)";
+
+                                int padding = progressLength - progressMessage.Length;
+                                if (padding > 0)
+                                    progressMessage += new string(' ', padding);
+
+                                Console.Write(progressMessage);
+                                progressLength = progressMessage.Length;
+                            }
+
+                            memoryStream.Close();
+                            decompressionStream.Close();
+                        }
+                        catch (Exception ex)
+                        {
+                            goto retry;
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+        }));
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("\rDownload Progress: Completed!");
+        Thread.Sleep(100);
+        Console.ReadKey();
+    }
+}
---
+++ Project Moral 11.31 Installer/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+class Program
+{
+        static void Main(string[] args)
+    {;
+        var httpClient = new WebClient();
+
+        List<string> versions = JsonConvert.DeserializeObject<List<string>>(httpClient.DownloadString(Globals.SeasonBuildVersion + "/versions.json"));
+        Console.Clear();
+
+        Console.Title = "Moral Build Installer";
+        Console.Write("Do you want to install version ");
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.Write("11.31");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write("?\nPlease type ");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write("'Yes' ");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write("or ");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write("'No'\n");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write(">> ");
+
+        var Version = Console.ReadLine();
+        var FN11 = 0;
+
+        switch (Version.ToLower())
+        {
+            case "yes":
+                FN11 = 10;
+                break;
+            case "no":
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Closing...");
+                Console.Out.Flush();
+                Thread.Sleep(2000);
+                return;
+            default:
+                Main(args);
+                return;
+        }
+
+        var targetVersion = versions[FN11].Split("-")[1];
+        var manifestUrl = $"{Globals.SeasonBuildVersion}/{targetVersion}/{targetVersion}.manifest";
+        var manifest = JsonConvert.DeserializeObject<FileManifest.ManifestFile>(httpClient.DownloadString(manifestUrl));
+
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.Write("Please enter a game folder location: ");
+        Console.ForegroundColor = ConsoleColor.White;
+        var targetPath = Console.ReadLine();
+        Console.WriteLine();
+
+        Installer.Download(manifest, targetVersion, targetPath).GetAwaiter().GetResult();
+    }
+}
+
+internal class Globals
+{
+    public const string SeasonBuildVersion = "https://manifest.fnbuilds.services";
+    public const int CHUNK_SIZE = 536870912 / 8;
+}
+
+internal class FileManifest
+{
+    public class ChunkedFile
+    {
+        public List<int> ChunksIds = new();
+        public string File = string.Empty;
+        public long FileSize = 0;
+    }
+
+    public class ManifestFile
+    {
+        public string Name = string.Empty;
+        public List<ChunkedFile> Chunks = new();
+        public long Size = 0;
+    }
+    internal class ConvertStorageSize
+    {
+        public static string FormatBytesWithSuffix(long bytes)
+        {
+            string[] Suffix = { "B", "KB", "MB", "GB", "TB" };
+            int i;
+            double dblSByte = bytes;
+            for (i = 0; i < Suffix.Length && bytes >= 1024; i++, bytes /= 1024)
+            {
+                dblSByte = bytes / 1024.0;
+            }
+
+            return string.Format("{0:0.##} {1}", dblSByte, Suffix[i]);
+        }
+    }
+}
